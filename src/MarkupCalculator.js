4e84842d90c3fe2a1bd6e6b87e9e62906df415cd
Phong Huynh <phuynh@architech.ca>
2016-10-26 20:55:12 -0400

    Add calculate worker fee function

--- src/MarkupCalculator.js
+++ src/MarkupCalculator.js
@@ -5,6 +5,11 @@ class MarkupCalculator {
     const flatFee = 0.05;
     return Math.round(markup * flatFee * 100) / 100;
   }
+
+  calculateWorkerFee(flatFee, numberOfWorkers) {
+    const workerFee = 0.012;
+    return Math.round(flatFee * (workerFee * numberOfWorkers) * 100) / 100;
+  }
 }
 
 module.exports = MarkupCalculator;

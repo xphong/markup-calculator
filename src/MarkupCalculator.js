83f189481b5669ab0e5723c237c1ea9cc838642a
Phong Huynh <phuynh@architech.ca>
2016-10-26 21:12:51 -0400

    Add calculate material fee function

--- src/MarkupCalculator.js
+++ src/MarkupCalculator.js
@@ -11,6 +11,26 @@ class MarkupCalculator {
     return this.roundToTwoDecimals(flatFee * (workerFee * numberOfWorkers));
   }
 
+  calculateMaterialFee(flatFee, materialType) {
+    const drugsFee = 0.075;
+    const foodFee = 0.13;
+    const electronicsFee = 0.02;
+
+    let materialFee;
+
+    if (materialType === 'drugs') {
+      materialFee = drugsFee;
+    } else if (materialType === 'food') {
+      materialFee = foodFee;
+    } else if (materialType === 'electronics') {
+      materialFee = electronicsFee;
+    } else {
+      materialFee = 0;
+    }
+
+    return materialFee === 0 ? this.roundToTwoDecimals(flatFee) : this.roundToTwoDecimals(flatFee * materialFee);
+  }
+
   roundToTwoDecimals(num) {
     return Math.round(num * 100) / 100;
   }

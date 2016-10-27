ac064ee8b76862e8b2e795d67df49d46ecf32026
Phong Huynh <phuynh@architech.ca>
2016-10-26 20:09:45 -0400

    Set up base files

---
+++ src/markupCalculator.spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const chai = require('chai').expect;
+const MarkupCalculator = require('./MarkupCalculator');
+
+describe('Markup Calculator', () => {
+  let markupCalculator;
+
+  beforeEach(() => {
+    markupCalculator = new MarkupCalculator();
+  });
+
+});

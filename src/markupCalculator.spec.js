0caf293c44273f2e21bd400918825ce11cde8b09
Phong Huynh <phuynh@architech.ca>
2016-10-26 20:31:21 -0400

    Create flat fee test

--- src/markupCalculator.spec.js
+++ src/markupCalculator.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const chai = require('chai').expect;
+const expect = require('chai').expect;
 const MarkupCalculator = require('./MarkupCalculator');
 
 describe('Markup Calculator', () => {
@@ -8,6 +8,12 @@ describe('Markup Calculator', () => {
 
   beforeEach(() => {
     markupCalculator = new MarkupCalculator();
+  });
+
+  it('should calculate flat markup fee', () => {
+    expect(markupCalculator.calculateFlatFee(1299.99)).to.equal(65.00);
+    expect(markupCalculator.calculateFlatFee(5432.00)).to.equal(271.60);
+    expect(markupCalculator.calculateFlatFee(12456.95)).to.equal(622.85);
   });
 
 });

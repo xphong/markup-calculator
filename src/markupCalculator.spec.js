d2993c931e3a26c8c0dc731882fdfcb2269b6acb
Phong Huynh <phuynh@architech.ca>
2016-10-26 20:52:53 -0400

    Create worker fee test

--- src/markupCalculator.spec.js
+++ src/markupCalculator.spec.js
@@ -4,6 +4,10 @@ const expect = require('chai').expect;
 const MarkupCalculator = require('./MarkupCalculator');
 
 describe('Markup Calculator', () => {
+  const basePrice1 = 1299.99;
+  const basePrice2 = 5432.00;
+  const basePrice3 = 12456.95;
+
   let markupCalculator;
 
   beforeEach(() => {
@@ -11,9 +15,19 @@ describe('Markup Calculator', () => {
   });
 
   it('should calculate flat markup fee', () => {
-    expect(markupCalculator.calculateFlatFee(1299.99)).to.equal(65.00);
-    expect(markupCalculator.calculateFlatFee(5432.00)).to.equal(271.60);
-    expect(markupCalculator.calculateFlatFee(12456.95)).to.equal(622.85);
+    expect(markupCalculator.calculateFlatFee(basePrice1)).to.equal(65.00);
+    expect(markupCalculator.calculateFlatFee(basePrice2)).to.equal(271.60);
+    expect(markupCalculator.calculateFlatFee(basePrice3)).to.equal(622.85);
+  });
+
+  it('should calculate worker fee', () => {
+    let flatFee1 = basePrice1 + markupCalculator.calculateFlatFee(basePrice1);
+    let flatFee2 = basePrice2 + markupCalculator.calculateFlatFee(basePrice2);
+    let flatFee3 = basePrice3 + markupCalculator.calculateFlatFee(basePrice3);
+
+    expect(markupCalculator.calculateWorkerFee(flatFee1, 3)).to.equal(49.14);
+    expect(markupCalculator.calculateWorkerFee(flatFee2, 1)).to.equal(68.44);
+    expect(markupCalculator.calculateWorkerFee(flatFee3, 4)).to.equal(627.83);
   });
 
 });

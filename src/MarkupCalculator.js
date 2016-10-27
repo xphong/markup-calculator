97a7d094232523e1e8549152cded666bcf47d485
Phong Huynh <phuynh@architech.ca>
2016-10-26 20:36:54 -0400

    Add flat fee function

--- src/MarkupCalculator.js
+++ src/MarkupCalculator.js
@@ -1,7 +1,10 @@
 'use strict';
 
 class MarkupCalculator {
-
+  calculateFlatFee(markup) {
+    const flatFee = 0.05;
+    return Math.round(markup * flatFee * 100) / 100;
+  }
 }
 
 module.exports = MarkupCalculator;
